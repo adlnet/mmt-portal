ffe43a565d5e01615f30f153868c553b77b45cf7
Eric Chang <echang3@deloitte.com>
2025-10-07 14:49:16 -0700

    Add delete ai user hook

---
+++ src/hooks/useDeleteAacademicInstituteUsers.js
@@ -0,0 +1,30 @@
+'use strict';
+
+import { useMutation, useQueryClient } from 'react-query';
+import { academicInstituteUsers } from '@/config/endpoints';
+import { axiosInstance } from '@/config/axiosConfig';
+/**
+ * @function useDeleteAcademicInstituteUser
+ * @description Hook to delete a user from AI by updating the entire user list
+ * @returns {useMutation}
+ */
+export function useDeleteAcademicInstituteUsers() {
+  const queryClient = useQueryClient();
+  
+  return useMutation(
+    ({ instituteId, updatedUsers }) => 
+      axiosInstance.put(`${academicInstituteUsers}${instituteId}/`, {
+        institute: instituteId,
+        members: updatedUsers.members,
+        administrators: updatedUsers.administrators
+      }),
+    {
+      onSuccess: () => {
+        queryClient.invalidateQueries(['academicInstituteUsers']);
+      },
+      onError: (err) => {
+        console.error('Error deleting user:');
+      },
+    }
+  );
+}
